cf29a6b975e40b0142a133d94eab1084be654b68
buggyGrandma <alireza.kh3221@gmail.com>
2024-02-25 09:19:35 +0330

    carousel ref

--- src/components/homePage/carousel.tsx
+++ src/components/homePage/carousel.tsx
@@ -20,14 +20,14 @@ const Carousel = ({ input }: { input: ReactNode[] }) => {
 	}
 	console.log(selected)
 	return (
-		<div className='relative '>
+		<div className='max relative w-full'>
 			<button
 				onClick={handlePrev}
 				className='absolute right-0 top-1/2 z-10 -translate-y-1/2 p-5'>
 				<Arrow className=' text-secondary hover:text-primary' direction='right' />
 			</button>
 			<div className='embla ' ref={emblaRef}>
-				<div className='embla__container m-10 max-w-7xl  overflow-visible   '>
+				<div className='embla__container m-10 max-w-7xl  overflow-visible'>
 					{input.map((item, i) => {
 						return (
 							<div
