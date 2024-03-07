abd25d0e82b44210dc1f720b45d5f13da88c1599
buggyGrandma <alireza.kh3221@gmail.com>
2024-03-07 21:35:54 +0330

    sign in click handled

--- src/components/SignInBtn.tsx
+++ src/components/SignInBtn.tsx
@@ -1,11 +1,14 @@
 "use client"
+import Link from "next/link"
 import Profile from "~/lib/icons/profile"
 
 export const SignInBtn: React.FC = () => {
 	return (
-		<button className='flex items-center rounded-lg bg-transparent text-sm font-bold text-secondary'>
-			<Profile />
-			<span className='ms-4 h-4'>ورود / ثبت نام</span>
-		</button>
+		<Link href='/signin'>
+			<button className='flex items-center rounded-lg bg-transparent text-sm font-bold text-secondary'>
+				<Profile />
+				<span className='ms-4 h-4'>ورود / ثبت نام</span>
+			</button>
+		</Link>
 	)
 }
